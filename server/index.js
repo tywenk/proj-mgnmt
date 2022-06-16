d6fe4a8940de53c0f76aa23975bb955f0418b992
Tywen Kelly <tywen.kelly@gmail.com>
2022-06-15 22:29:45 -0700

    adds mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express")
+const colors = require("colors")
 require("dotenv").config()
 const { graphqlHTTP } = require("express-graphql")
 const schema = require("./schema/schema")
+const connectDB = require("./config/db")
 
 const port = process.env.PORT || 5200
 
 const app = express()
+
+connectDB()
+
 app.listen(port, console.log(`Server Running on Port ${port}`))
 app.use(
 	"/graphql",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,7 @@
 const { projects, clients } = require("../sampleData")
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require("graphql")
 
+//Client Type
 const ClientType = new GraphQLObjectType({
 	name: "Client",
 	fields: () => ({
@@ -12,12 +13,20 @@ const ClientType = new GraphQLObjectType({
 	}),
 })
 
+//Project Type
 const ProjectType = new GraphQLObjectType({
 	name: "Project",
 	fields: () => ({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		description: { type: GraphQLString },
+		status: { type: GraphQLString },
+		client: {
+			type: ClientType,
+			resolve(parent, args) {
+				return clients.find((client) => client.id === parent.clientId)
+			},
+		},
 	}),
 })
 
